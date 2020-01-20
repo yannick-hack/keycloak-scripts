53d7a607385975c89cd02c5a778d4b677276f159
yannick-hack <60113494+yannick-hack@users.noreply.github.com>
2020-01-20 21:40:56 +0100

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -8,5 +8,5 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt -y update
 apt-cache -y policy docker-ce
 apt -y install docker-ce
-docker run -d -p 9001:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name keycloak jboss/keycloak    
+docker run -d -p 9001:8080 -e PROXY_ADDRESS_FORWARDING=true KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name keycloak jboss/keycloak    
 docker exec keycloak /opt/jboss/keycloak/bin/jboss-cli.sh --connect "/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding, value=true)"
